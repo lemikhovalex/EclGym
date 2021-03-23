eefb407748a532847431836a0ce303fe22090423
Lemikhov Alex <lemikhovalex@gmail.com>
2021-03-23 12:54:09 +0300

    maybe it

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name='ecl_gym',
     author_email='lemikhovalex@gmail.com',
     description='Package with gym-like env for petroleum reservoir simulation',
-    packages=['ecl_gym']
-    ),
+    packages=find_namespace_packages(
+        where=['ecl_gym', 'ecl_gym.*']
+    )
 )
