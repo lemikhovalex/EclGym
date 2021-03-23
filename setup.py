b4a03b23f8164e49227838c58cc800cab3476782
lemikhovalex <42550446+lemikhovalex@users.noreply.github.com>
2021-03-23 12:32:06 +0300

    is it enough for packaging?

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+import pkg_resources
+
+setup(
+    author_email='lemikhovalex@gmail.com',
+    description='Package with gym-like env for petroleum reservoir simulation',
+    packages=['ecl_gym']
+)
